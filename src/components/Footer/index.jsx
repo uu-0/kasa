c2634bd914632e617b81e7e555bd6a1fc4993473
unknown <louisepop14@gmail.com>
2024-11-15 14:45:37 +0100

    étape 4: création du footer #4

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,12 @@
+import logo from '../../assets/kasa-logo-white.svg'
+
+import '../../styles/css/footer.css'; 
+
 function Footer() {
     return (
         <footer>
-            <p>footer</p>
+            <img className='kasa-logo' src={logo} alt={`kasa logo`} />
+            <p>© 2020 Kasa. All rights reserved</p>
         </footer>
     )
 }
