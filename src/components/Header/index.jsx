8ee3b3bc130ef2efe5ad356b63a04635682abd3f
unknown <louisepop14@gmail.com>
2024-11-15 14:23:36 +0100

    étape 4: création de l'header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom'
+import logo from '../../assets/kasa-logo.png'
+
+import '../../styles/css/header.css'; 
 
 function Header() {
     return (
-        <nav>
-            <p>nav</p>
-            <Link to="/">Home</Link>
-            <Link to="/logement">Logement</Link>
-            <Link to="/a-propos">À Propos</Link>
-        </nav>
+        <header>
+            <img className='kasa-logo' src={logo} alt={`kasa logo`} />
+            <nav>
+                <Link class="kasa-nav-link kasa-nav-link-active" to="/">Accueil</Link>
+                <Link class="kasa-nav-link" to="/a-propos">À Propos</Link>
+            </nav>
+        </header>
     )
 }
 
