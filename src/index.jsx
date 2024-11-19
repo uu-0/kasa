d567602aac2e76b2393c0c0ccebc3521e43271ac
unknown <louisepop14@gmail.com>
2024-11-19 17:07:04 +0100

    étape 4: page d'accueil #4

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-import App from './components/App';
 import Header from './components/Header';
-import Home from './pages/Home';
+import Accueil from './pages/Accueil';
 import Logement from './pages/Logement';
 import APropos from './pages/APropos';
 import Error from './components/Error';
 import Footer from './components/Footer';
 
-import './styles/index.css';
+import './styles/css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router> 
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/logement" element={<Logement />} />
+        <Route path="/" element={<Accueil />} />
+        <Route path="/logement/:id" element={<Logement />} />
         <Route path="/a-propos" element={<APropos />} />
         <Route path="*" element={<Error />} />
       </Routes>
