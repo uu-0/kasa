e36c6aeb6587d394adc30268e6ddb9bb32b0bc10
unknown <louisepop14@gmail.com>
2024-11-22 14:30:22 +0100

    modification banner #4

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-
 import '../styles/css/banner.css'
 
-
-function Banner() {
+function Banner({ backgroundImage, showText = true }) {
     return (
-            <React.Fragment>
-                <div className='kasa-accueil-bloc'></div>
-                <div className='kasa-accueil-bloc-filter'>
+        <React.Fragment>
+            <div 
+                className='kasa-accueil-bloc' 
+                style={{ backgroundImage: `url(${backgroundImage})` }}
+            />
+            <div className='kasa-accueil-bloc-filter'>
+                {showText && (
                     <div className="text-container">
                         <span className="outline">Chez vous, partout et ailleurs</span>
                         <span className="fill">Chez vous, partout et ailleurs</span>
                     </div>
-                </div>
-            </React.Fragment>
+                )}
+            </div>
+        </React.Fragment>
     )
 }
 
